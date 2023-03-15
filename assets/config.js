f0247a6f4a809dc7a5a234d362ddcbda7eeda87b
chnd372 <81694889+chnd372@users.noreply.github.com>
2023-03-15 10:59:25 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,42 +1,22 @@
 let config = {
-  "Title": "Your Name - Links",
-  "Name": "Your Name",
-  "Description": "Your Description",
+  "Title": "Ican - Links",
+  "Name": "Chandra T Mairafela",
+  "Description": "Seorang pemuda yang bermimpi menjadi yang terbaik dari yang terbaik",
   "links": [
     {
       "Title": "Website",
-      "URL": "http://www.harindu.dev",
+      "URL": "https://weizman.tech",
       "icon_classes": "fas fa-link",
     },
     {
       "Title": "Github",
-      "URL": "https://github.com/harindulk",
+      "URL": "https://github.com/chnd372",
       "icon_classes": "fab fa-github",
     },
     {
-      "Title": "Play Store",
-      "URL": "https://play.google.com/store/apps/dev?id=6729187126243636577",
-      "icon_classes": "fab fa-google-play"
-    },
-    {
-      "Title": "PC Games",
-      "URL": "https://harindulk.itch.io/",
-      "icon_classes": "fab fa-itch-io"
-    },
-    {
-      "Title": "Blog",
-      "URL": "blog-test",
-      "icon_classes": "fas fa-blog"
-    },
-    {
       "Title": "Twitter",
-      "URL": "https://twitter.com/Harindu_Fonseka",
-      "icon_classes": "fab fa-twitter"
-    },
-    {
-      "Title": "LinkedIn",
-      "URL": "https://www.linkedin.com/in/harindulk/",
-      "icon_classes": "fab fa-linkedin"
+      "URL": "https://twitter.com/ChandraRrc471",
+      "icon_classes": "fab fa-twitter", 
     },
     {
       "Title": "Youtube",
